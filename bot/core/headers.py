4e2f7b60a82d157608d58474d65253505d325db8
Vanhbaka <vanhbaka2k7@gmail.com>
2024-10-17 21:15:21 +0700

    Update headers.py

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -3,15 +3,15 @@ headers = {
     'accept-language': 'en-ID,en-US;q=0.9,en;q=0.8,id;q=0.7',
     "pragma": "no-cache",
     "cache-control": "no-cache",
-    'origin': 'https://cexp8.cex.io',
+    'origin': 'https://app.cexptap.com',
     'priority': 'u=1, i',
-    'referer': 'https://cexp8.cex.io/',
+    'referer': 'https://app.cexptap.com/',
     'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
+    'sec-fetch-site': 'same-origin',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
     'x-appl-version': '0.11.0'
 }
